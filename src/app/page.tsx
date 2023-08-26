05b38fcd7ad520ca0e0bab7ac64dba8d127ff36b
Leslie Hoare <iam@lesleh.co.uk>
2023-08-27 00:01:06 +0100

    Fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
 import prisma from "@/lib/prisma";
-import { Quiz } from "@/components/Quiz";
-
-const quiz = {
-  title: "Sample Quiz",
-  description: "Sample description",
-  questions: [
-    {
-      question: "Sample Question",
-      options: [
-        "Sample answer 1",
-        "Sample answer 2",
-        "Sample answer 3",
-        "Sample answer 4",
-      ],
-      correctOption: "Sample answer 3",
-    },
-  ],
-};
+import Link from "next/link";
 
 export default async function Home() {
   const quizzes = await prisma.quiz.findMany();
 
   return (
     <main className="max-w-4xl mx-auto my-8">
-      {quizzes.map((quizz) => (
-        <div>{quizz.title}</div>
+      {quizzes.map((quiz) => (
+        <div key={quiz.id}>
+          <Link href={`/quiz/${quiz.id}`}>{quiz.title}</Link>
+        </div>
       ))}
     </main>
   );
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,7 +8,11 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL_NON_POOLING,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
