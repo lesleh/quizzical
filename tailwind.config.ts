55580fca840d866c83c41525f738dd7ddc1a8b33
Leslie Hoare <iam@lesleh.co.uk>
2023-08-27 13:40:16 +0100

    Use dracula theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   daisyui: {
-    themes: ["autumn"],
+    themes: ["dracula"],
   },
   theme: {},
   plugins: [require("daisyui")],
