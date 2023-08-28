b63997c7c2a3fb88ab27f8727829f325cb08db71
Leslie Hoare <iam@lesleh.co.uk>
2023-08-28 23:38:38 +0100

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,16 @@ export default async function Home() {
 
   return (
     <main className="max-w-4xl mx-auto my-8">
-      {quizzes.map((quiz) => (
-        <div key={quiz.id}>
-          <Link href={`/quiz/${quiz.id}`}>{quiz.title}</Link>
-        </div>
-      ))}
+      <h1 className="text-3xl font-bold text-primary">Quizzical</h1>
+      <ul className="list-disc list-inside">
+        {quizzes.map((quiz) => (
+          <li key={quiz.id} className="my-2">
+            <Link href={`/quiz/${quiz.id}`} className="underline">
+              {quiz.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
