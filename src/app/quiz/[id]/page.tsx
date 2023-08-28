6e0a23d2b10ca720772908e001869f5394390d76
Leslie Hoare <iam@lesleh.co.uk>
2023-08-28 23:38:49 +0100

    Add back link to quizzes

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { Quiz } from "@/components/Quiz";
 import prisma from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function QuizPage({
   params: { id },
@@ -19,7 +20,10 @@ export default async function QuizPage({
   }
 
   return (
-    <main className="min-h-full max-w-4xl mx-auto p-4 grid grid-rows-[auto,_auto_,1fr] gap-4">
+    <main className="min-h-full max-w-4xl mx-auto p-4 grid grid-rows-[auto,_auto,_auto_,1fr] gap-4">
+      <Link href="/" className="underline">
+        Back
+      </Link>
       <Quiz quiz={quiz} />
     </main>
   );
